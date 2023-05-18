6621d47ecc507c6e0c4c8fe8ca9296d56499466d
Tibor Sándor <tiborsandor2001@gmail.com>
2023-05-18 14:55:24 +0200

    Add spell checking autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Turn on spell check for these filetypes
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = "hu,en_us,ru"
+  end,
+})
