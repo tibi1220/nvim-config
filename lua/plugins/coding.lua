42ba2e7ecd79a2c6588ad0acd18864d4a5cb6e67
Tibor Sándor <tiborsandor2001@gmail.com>
2023-07-03 11:43:55 +0200

    Add own snippets

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,9 +6,11 @@ return {
   -----------------------------------------------------------------------------
   {
     "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
+        require("luasnip.loaders.from_vscode").load { paths = "~/.config/nvim/lua/snippets" }
         require("luasnip.loaders.from_vscode").lazy_load()
       end,
     },
